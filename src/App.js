67352ba783f9a901ec21cc7ecd69a24d861638b0
HoangFuc <daylafuc@gmail.com>
2023-09-07 15:44:50 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Login from './login/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './home/Home';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" Component={Login} ></Route>
+            <Route path="/home" Component={Home}></Route>
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
--- src/login/Login.js
+++ src/login/Login.js
@@ -3,10 +3,13 @@ import logo from './logo.jpg'
 import "./Login.scss"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import { Navigate } from 'react-router-dom';
+
 class Login extends React.Component {
     state = {
         username: '',
-        password: ''
+        password: '',
+        validate: false
     }
     handleInputUserName = (event) => {
         this.setState({
@@ -21,18 +24,27 @@ class Login extends React.Component {
     }
 
     validateAccount = async () => {
-        let user = await axios.get("http://localhost:3000/accounts");
-        console.log(this.state.username);
-        console.log(user.data.username);
+        let data = await axios.post('http://localhost:3000/auth/login', {
+            username: this.state.username,
+            password: this.state.password
+        })
+
+        if (data.status === 200) {
+            this.setState({
+                validate: true
+            })
+        }
     }
     render() {
+        let { validate } = this.state;
         return (
             <>
                 <img src={logo} alt="Logo" />
                 <div className='login'>
                     <input type='text' placeholder='Nhap ten tai khoan' onChange={(event) => this.handleInputUserName(event)}></input> <br />
                     <input type='password' placeholder='Nhap mat khau' onChange={(event) => this.handleInputPassword(event)}></input> <br />
-                    <button type='button' onClick={this.validateAccount()}>Dang Nhap</button>
+                    <button type='button' onClick={() => this.validateAccount()}>Dang Nhap</button>
+                    {validate && (< Navigate to="/home" />)}
                 </div >
             </>
         )
