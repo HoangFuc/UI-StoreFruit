d272867304cd17c31e112d5df10d8787ece6c9f6
HoangFuc <daylafuc@gmail.com>
2023-09-25 09:27:19 +0700

    update logging

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -3,18 +3,33 @@ import { NavLink } from "react-router-dom";
 import "./Nav.scss"
 
 class Nav extends React.Component {
+
+    handleShow = () => {
+        return (
+            <ul>
+                <li>
+                    a
+                </li>
+                <li>
+                    b
+                </li>
+            </ul>
+        )
+    }
+
     render() {
-        let validate = this.props.validate;
-        console.log("check validate ", validate)
+        let { validate } = this.props;
         return (
             <>
                 <NavLink to="/home" activeClassName="active">Home</NavLink>
                 <NavLink to="/abc" activeClassName="active" >A</NavLink>
                 <NavLink to="/xys" activeClassName="active">B</NavLink>
                 <NavLink to="/z" activeClassName="active">C</NavLink>
-                {validate && validate.length > 0
+                {validate.validate == "true"
                     ?
-                    <div>a</div>
+                    <nav id="login">
+                        <img src={require(`../assets/Phuc.jpg`)} type="button" onClick={() => this.handleShow()} />
+                    </nav>
                     :
                     <nav id="login">
                         <NavLink to="/login" activeClassName="active">Login</NavLink>
